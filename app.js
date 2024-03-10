022f2659c0a970603f856b501ab786864422afc2
Fahadarshadkhan <154579872+Fahadarshadkhan@users.noreply.github.com>
2024-03-10 16:23:57 +0500

    Add files via upload
    variables-fahad

---
+++ app.js
@@ -0,0 +1,24 @@
+var username = "fahad Arshad"
+document.write (username); 
+
+var message =  "hello world"
+document.write (message);
+alert ("hello world")
+
+var username = "fahad Arshad"
+document.write (username);
+
+var age =  "26 years old"
+document.write (age)
+
+var qualification =  "B com"
+document.write (qualification)
+
+var extraqualification =  "B.B.A"
+document.write (extraqualification)
+
+var JOB =  "AL-BARAKA BANK"
+document.write (JOB)
+
+var EXPERIANCE =  "15 years"
+document.write (EXPERIANCE)
